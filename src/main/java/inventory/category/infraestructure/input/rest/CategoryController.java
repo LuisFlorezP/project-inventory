d9d7910990ca3d689239b1c776a02834f8797305
Luis Angel Florez Palacio <braian1k3@gmail.com>
2024-07-10 17:19:58 -0500

    build: enabled cors.

--- src/main/java/inventory/category/infraestructure/input/rest/CategoryController.java
+++ src/main/java/inventory/category/infraestructure/input/rest/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import static inventory.common.application.utils.Constants.RQ_UUID;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/categories")
 @Tag(name = "Category Controller", description = "Controller to the Category entity")
